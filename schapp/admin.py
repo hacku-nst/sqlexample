030506d5b898c872b19e2079262f2072f0dd66c5
Troy Scott <troy_pdx@fastmail.fm>
2016-11-18 00:10:09 +0000

    rev0.3

--- schapp/admin.py
+++ schapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import School
+from .models import Performance
+
+admin.site.register(School)
+admin.site.register(Performance)
